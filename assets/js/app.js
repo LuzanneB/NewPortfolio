fd6308b733342aca207982c81d199a7139c05b01
Luzanne Burt <luzannea@gmail.com>
2019-09-18 12:24:32 -0400

    added animation and aboutme filler

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,27 +1,30 @@
 $(document).ready(function(){
     console.log ("ready!");
+
+// initialize parallax and sidenav
     $(".parallax").parallax();
     $(".sidenav").sidenav();
 
+// go to top element
+// grab the button
+    const gototop = $("#goto");
+// set rules for when to show and hide the button
+    $(window).scroll(function() {
+       if ($(window).scrollTop() > 20) {
+         gototop.show();
+       } else {
+         gototop.hide();
+       }
+     });
+// scroll to the top of the page when the button is clicked 
+     gototop.on("click", function(e){
+         e.preventDefault();
+         $("html,body").animate({scrollTop:0},"300");
+     });
+   
 
 
-    
- const gototop = $("#goto");
 
-
-
- $(window).scroll(function() {
-    if ($(window).scrollTop() > 20) {
-      gototop.show();
-    } else {
-      gototop.hide();
-    }
-  });
-
-  gototop.on("click", function(e){
-      e.preventDefault();
-      $("html,body").animate({scrollTop:0},"300");
-  });
 
 
 

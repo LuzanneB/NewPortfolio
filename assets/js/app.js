8edffdd4fbd4277b82702cdb666096f8e096218d
Luzanne Burt <luzannea@gmail.com>
2019-09-18 09:08:29 -0400

    go to top added

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,10 +1,27 @@
 $(document).ready(function(){
     console.log ("ready!");
-    $('.parallax').parallax();
-    $('.sidenav').sidenav();
- 
+    $(".parallax").parallax();
+    $(".sidenav").sidenav();
 
 
+
+    
+ const gototop = $("#goto");
+
+
+
+ $(window).scroll(function() {
+    if ($(window).scrollTop() > 20) {
+      gototop.show();
+    } else {
+      gototop.hide();
+    }
+  });
+
+  gototop.on("click", function(e){
+      e.preventDefault();
+      $("html,body").animate({scrollTop:0},"300");
+  });
 
 
 
